50bcfad34f9dacb0a127972fb55a56cf8c432bec
marcellobeltrami <marcello.beltrami@gmail.com>
2024-05-06 23:32:08 +0100

    added QC

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,11 +1,6 @@
 from vcf import Reader, Writer
 
 
-def quality_filtering(vcf_file): 
-
-    return 
-    
-
 
 #Function that generates relevant chromosomes. Preset is for humans. 
 def chrom_gen(chromosome=25):
@@ -44,7 +39,7 @@ def chr_filtering(vcf_location_path='all_samples_INDELS.vcf', output_location_pa
     return 0 
 
 
-def mutation_filter(vcf_qual_filtered, res, sens, vcf_output='./differential_SNPs.vcf',sens_threshold=1, res_threshold=2):
+def mutation_filter(vcf_qual_filtered, res, sens, vcf_output='./differential_SNPs.vcf',sens_threshold=1, res_threshold=2, FREQ_thresh=10, ADP_thres=10, GQ_thresh=30):
     resistant_set = set(res)
     sensitive_set = set(sens)
 
@@ -56,6 +51,7 @@ def mutation_filter(vcf_qual_filtered, res, sens, vcf_output='./differential_SNP
     # For each record in file, accesses the sample INFO
     for record in vcf_reader:
         
+
         if chrom_checked != record.CHROM:
                 print(chrom_checked, "done!")
                 print(record.CHROM,"being checked...")
@@ -64,36 +60,53 @@ def mutation_filter(vcf_qual_filtered, res, sens, vcf_output='./differential_SNP
         res_GP = []
         sens_GP = []
     
-        # 0/1-1/0 heterozygous mutation, 1/1 homozygous mutation  
-        #Splits info and puts them in dictionary "record_dictionary" based on sample name
-        for sample_res in resistant_set: 
-            genotype_res = str(record.genotype(sample_res).data.GT)
-            
-            if genotype_res == "0/1" or genotype_res == "1/0": 
-                res_GP.append(genotype_res)
-            elif genotype_res == "1/1": 
-                res_GP.append(genotype_res)
-            else: 
-                pass
         
-        #Checks sensitive genotypes for each sample and appends them to a list if they are heterozygous or homozygous mutations.  
-        for sample_sens in sensitive_set: 
-            genotype_sens = str(record.genotype(sample_sens).data.GT)
-
-            if genotype_sens == "0/1" or genotype_sens == "1/0": 
-                sens_GP.append(genotype_sens)
-            elif genotype_sens == "1/1": 
-                sens_GP.append(genotype_sens)
-            else: 
-                pass
-    
+        #Checks for errors in filter and threshold of ADP (Average per-sample depth based on Phred score). 
+        if record.FILTER == [] and record.INFO.get('ADP') >= ADP_thres:
         
-        #Determines differential mutations by using length of respective lists and saves record to a file.
+            for sample_res in resistant_set: 
+                genotype_res = str(record.genotype(sample_res).data.GT)
+                genotype_qual = str(record.genotype(sample_res).data.GQ)
+                
+                try:
+                    # 0/1-1/0 heterozygous mutation, 1/1 homozygous mutation  
+                    #Splits info and puts them in dictionary "record_dictionary" based on sample name
+                    
+                    #Checks for genotype quality.
+                    if genotype_qual >= GQ_thresh:
 
-        if len(res_GP) >= res_threshold and len(sens_GP) <= sens_threshold:
+                        FREQ= str(record.genotype(sample_res).data.FREQ)
+                        FREQ_fl = float(FREQ.replace("%", "")) 
+                        #Checks for FREQUENCY threshold.
+                        if FREQ_fl >= FREQ_thresh: 
+                            if genotype_res == "0/1" or genotype_res == "1/0": 
+                                res_GP.append(genotype_res)
+                            elif genotype_res == "1/1": 
+                                res_GP.append(genotype_res)
+                            else: 
+                                pass
+                except ValueError:
+                    pass  
+
+                
             
-           
-            differential_mutations.write_record(record)
+            #Checks sensitive genotypes for each sample and appends them to a list if they are heterozygous or homozygous mutations.  
+            for sample_sens in sensitive_set: 
+                genotype_sens = str(record.genotype(sample_sens).data.GT)
+
+                if genotype_sens == "0/1" or genotype_sens == "1/0": 
+                    sens_GP.append(genotype_sens)
+                elif genotype_sens == "1/1": 
+                    sens_GP.append(genotype_sens)
+                else: 
+                    pass
+        
+            
+            #Determines differential mutations by using length of respective lists and saves record to a file.
+
+            if len(res_GP) >= res_threshold and len(sens_GP) <= sens_threshold:
+                
+                differential_mutations.write_record(record)
 
     #Lists are emptied for each record to save memory.
     sens_GP = []
@@ -102,10 +115,10 @@ def mutation_filter(vcf_qual_filtered, res, sens, vcf_output='./differential_SNP
     print(chrom_checked,"done!")
 
 # Example usage
-vcf_file = "./test.vcf"
+vcf_file = "/home/marcello/Thesis_dev/VCF_annotator/scripts/test.vcf"
 sample_group_res = ["BT594", "MBT357", "MBT373"]
 sample_group_sens = ["BT972", "BT241", "BT935", "MBT168"]
 
 mutation_filter(vcf_file, sample_group_res, sample_group_sens)
-#print("Mutations found in two or more samples in group1 but not in group2:")
+
 
---
+++ test.py
@@ -0,0 +1,21 @@
+from vcf import Reader, Writer
+
+
+
+vcf_file = Reader(open("./scripts/test.vcf", 'r'))
+sample_group_res = ["BT594", "MBT357", "MBT373"]
+sample_group_sens = ["BT972", "BT241", "BT935", "MBT168"]
+
+
+for record in vcf_file: 
+    for sample in sample_group_res: 
+        print(str(record.FILTER == []))
+        try:
+            FREQ= str(record.genotype(sample).data.FREQ)
+            FREQ_fl = float(FREQ.replace("%", ""))
+            
+
+            if FREQ_fl > 50: 
+                print(record)
+        except ValueError:
+            pass  
