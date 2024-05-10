4fd8c4a5b6d78ecf512d36512270c81bde132de1
marcellobeltrami <marcello.beltrami@gmail.com>
2024-05-10 17:35:40 +0100

    quick-save

--- main_cli.py
+++ main_cli.py
@@ -14,19 +14,18 @@ if __name__ == "__main__":
     print("--------------------------------------------------------------------")
     #CLI interface
     main_parser = ArgumentParser(description='CLI interface for VCF annotator')
-    analysisis_parser= main_parser.add_subparsers(title="analysis")
-    analysis= analysisis_parser.add_parser(title="analysis", help="carries out VCF annotations and filtering")
+    
     # CLI analysisi interface
-    analysis.add_argument('-i', '--input_file', required=True, help='Path to input file')
-    analysis.add_argument('-o', '--output_file', required=True, help='Output file name. Insert an identifier for your file, this is NOT the output file path')
-    analysis.add_argument('-g', '--sample_groups', required=True, help='A TAB delimited file containing sample names found in VCF file. First column should be the normal, second one treatment')
-    analysis.add_argument('-temp','--temp_keep', type=str, default="n",help='Determines whether to keep intermediate files[y/n].')
-    analysis.add_argument('-nm','--normal_mutation_threshold', type=int, default=1,help='Mutations with genotypes above this values and lower than treatment GT will be removed ')
-    analysis.add_argument('-tm','--treatment_mutation_threshold', type=int, default=2,help='Mutations with genotypes above this values and lower than normal GT will be kept ')
+    main_parser.add_argument('-i', '--input_file', required=True, help='Path to input file')
+    main_parser.add_argument('-o', '--output_file', required=True, help='Output file name. Insert an identifier for your file, this is NOT the output file path')
+    main_parser.add_argument('-g', '--sample_groups', required=True, help='A TAB delimited file containing sample names found in VCF file. First column should be the normal, second one treatment')
+    main_parser.add_argument('-temp','--temp_keep', type=str, default="n",help='Determines whether to keep intermediate files[y/n].')
+    main_parser.add_argument('-nm','--normal_mutation_threshold', type=int, default=1,help='Mutations with genotypes above this values and lower than treatment GT will be removed ')
+    main_parser.add_argument('-tm','--treatment_mutation_threshold', type=int, default=2,help='Mutations with genotypes above this values and lower than normal GT will be kept ')
     
     
     # Parse arguments
-    args_analysis = analysis.parse_args()
+    args_analysis = main_parser.parse_args()
     samples_tab=str(args_analysis.sample_groups)
     input_file=str(args_analysis.input_file)
     temp_keep = args_analysis.temp_keep
@@ -34,31 +33,6 @@ if __name__ == "__main__":
     nml_thresh= int(args_analysis.normal_mutation_threshold)
     trt_thresh = int(args_analysis.treatment_mutation_threshold)
     
-
-    # CLI analysisi interface for purging files
-    purge_subparsers=main_parser.add_subparsers(title="purge")
-    purge = purge_subparsers.add_parser("purge", type=str, help="Purge files")
-    args_purge = purge.parse_args()
-    purge_arg=args_purge.purge
-    
-    json_file_cred = "./scripts/settings/credentials.json"
-    if purge_arg: 
-        confirm = input("Are you sure you want to remove login info and results? [y/n]: ")
-
-        if confirm=="y" or confirm=="Y":
-            print("Removing files....")
-            results_to_rm = listdir("./results/")    
-            for res in results_to_rm:
-                remove(res)
-                print(res, "removed.")
-
-            if path.exists(json_file_cred) == True:
-                remove(json_file_cred)
-            else: 
-                print("Credentials were not found,  hence were not removed.") 
-            
-            print("Done! Good-bye :)")
-            exit()
 
     #Functionality that allows to save credentials. 
     usr_glob=""
